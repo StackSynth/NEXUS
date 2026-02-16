4a984c4ff857b2c8901b80e6a4f77e72cb79f27d
Rejoe <gnanav948@gmail.com>
2026-02-16 23:09:08 +0530

    fix: homepage and app component, css centering, show homepage

---
+++ persional AI/src/App.jsx
@@ -0,0 +1,11 @@
+import Homepage from './Component/homepage';
+
+function App() {
+	return (
+		<div>
+			<Homepage />
+		</div>
+	);
+}
+
+export default App;
---
+++ persional AI/src/Component/homepage.jsx
@@ -0,0 +1,12 @@
+
+import './homepage.css';
+
+function Homepage() {
+  return (
+    <div className="heading">
+      <h1>Welcome to Personal AI</h1>
+    </div>
+  );
+}
+
+export default Homepage;
