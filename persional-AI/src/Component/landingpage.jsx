1f3fc722d8b573bcb7af9b9a1141e6ae976210d2
Rejoe <gnanav948@gmail.com>
2026-02-18 22:14:08 +0530

    Refactor: rename project folder from 'persional AI' to 'persional-AI' and update UI colors to indigo/purple theme

---
+++ persional-AI/src/Component/landingpage.jsx
@@ -0,0 +1,19 @@
+import './landingpage.css'
+import { VscAccount } from "react-icons/vsc";
+import { MdNotifications } from "react-icons/md";
+import { motion } from "framer-motion"
+
+function LandingPage() {
+  return (
+    <div>
+      <div className="section">
+        <div className="header">
+          
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default LandingPage
