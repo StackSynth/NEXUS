19f4a1f746812326bf60495b3f25b38b5796a3f9
Rejoe <gnanav948@gmail.com>
2026-02-25 00:03:32 +0530

    initial commit

--- persional-AI/src/Component/landingpage.jsx
+++ persional-AI/src/Component/landingpage.jsx
@@ -3,13 +3,16 @@ import './landingpage.css'
 function LandingPage() {
   return (
     <div>
-      <div className="section">
         <div className="container">
-          <h1>NEXUS</h1>
-
+          <div className="heading">
+             <h1>NEXUS</h1>
+          </div>
+          <div className="subheading">
+            <h2>Personal AI Assistant</h2>
+          </div>
+    
         </div>
       </div>
-    </div>
 
   );
 }
